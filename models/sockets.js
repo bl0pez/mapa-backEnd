5b8a832f0c25d72ff1720cd4f6090402f4f6c7fe
b0pez <blopez4434@gmail.com>
2022-10-30 17:50:37 -0300

    first commit

---
+++ models/sockets.js
@@ -0,0 +1,34 @@
+const Marcadores = require("./marcadores");
+
+class Sockets {
+    constructor(io) {
+        this.io = io;
+        this.marcadores = new Marcadores();
+        this.socketEvents();
+    }
+
+    socketEvents() {
+        //On connection
+        this.io.on('connection', (socket) => {
+            //Escucha evento: marcadores-activos
+            socket.emit('marcadores-activos', this.marcadores.activos);
+
+            //Emitir marcadores nuevos
+            socket.on('marcador-nuevo', (marcador) => {
+                this.marcadores.agregarMarcador(marcador);
+                socket.broadcast.emit('marcador-nuevo', marcador);
+            });
+
+            //Marcadore actualizado
+            socket.on('marcador-actualizado', (marcador) => {
+                this.marcadores.actualizarMarcador(marcador);
+                socket.broadcast.emit('marcador-actualizado', marcador);
+            });
+
+        });
+    }
+
+}
+
+
+module.exports = Sockets;
